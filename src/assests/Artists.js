0532954bbcf11aae457805f96db584813fb1fb38
Aryan Kumar <lci2020004@iiitl.ac.in>
2023-04-22 00:36:16 +0530

    Moved artists array in another file

---
+++ src/assests/Artists.js
@@ -0,0 +1,103 @@
+const artist = [
+    {
+      id: 1,
+      name: "(style of abraham bosschaert)",
+      link: "https://media.seedscienceai.com/media/EQjMHRN8vXFdxJcHwYBMmBtVXdo.webp?&width=500",
+    },
+    {
+      id: 2,
+      name: "(style of tim burton)",
+      link: "https://media.seedscienceai.com/media/URSVFlW7lhyQKj4jv5lKC-PZEFI.webp?&width=500",
+    },
+    {
+      id: 3,
+      name: "(style of m.c. escher)",
+      link: "https://media.seedscienceai.com/media/YnmphCm_f0QBueA2aziUrggKR5I.webp?&width=500",
+    },
+    {
+      id: 4,
+      name: "(style of tristan eaton)",
+      link: "https://media.seedscienceai.com/media/9XDdDL6qTVmN6iBgfH61OwpMwK4.webp?&width=500",
+    },
+    {
+      id: 5,
+      name: "(style of ernst haeckel)",
+      link: "https://media.seedscienceai.com/media/xrQSBzuRoPD7TKERo9PYhQsBaU4.webp?&width=500",
+    },
+    {
+      id: 6,
+      name: "(style of anton semenov)",
+      link: "https://media.seedscienceai.com/media/Qo488chxbN3VmrAfk7LyWN8cDb0.webp?&width=500",
+    },
+    {
+      id: 7,
+      name: "(style of matt groening)",
+      link: "https://media.seedscienceai.com/media/JkJTDG4z-W4u7sPLmZBBSLrVRDc.webp?&width=500",
+    },
+    {
+      name: "(style of vladimir kush)",
+      link: "https://media.seedscienceai.com/media/AeKjLRbwT-Q4tWa69KYJR24HrhU.webp?&width=500",
+    },
+    {
+      id: 8,
+      name: "(style of gaetano pesce)",
+      link: "https://media.seedscienceai.com/media/nDecmVV1albRCNSp7TH_s_4jhNE.webp?&width=500",
+    },
+    {
+      id: 9,
+      name: "(style of charles addams)",
+      link: "https://media.seedscienceai.com/media/KrcTHykbVq1AJv5E_HBAv89Hgd0.webp?&width=500",
+    },
+    {
+      id: 10,
+      name: "(style of antonio mancini)",
+      link: "https://media.seedscienceai.com/media/73IJ-zzpqB0FKtenJuZAFuy-BAw.webp?&width=500",
+    },
+    {
+      id: 11,
+      name: "(style of victor vasarely)",
+      link: "https://media.seedscienceai.com/media/1FeWejCrZMa2HjbaEgh1tdIhvFc.webp?&width=500",
+    },
+    {
+      id: 12,
+      name: "(style of john kenn mortensen)",
+      link: "https://media.seedscienceai.com/media/gSCqK8QHtqV0BQSiX63rHjL-3lA.webp?&width=500",
+    },
+    {
+      id: 13,
+      name: "(style of jim lee)",
+      link: "https://media.seedscienceai.com/media/tchSb4n2ai3I1GIsp7hwVh3ocS8.webp?&width=500",
+    },
+    {
+      id: 14,
+      name: "(style of dr. seuss)",
+      link: "https://media.seedscienceai.com/media/iNWt4M5zikoZ3GdJa-RKM4sdvyg.webp?&width=500",
+    },
+    {
+      id: 15,
+      name: "(style of matt groening)",
+      link: "https://media.seedscienceai.com/media/jMwGmNZGfpnNdwfmnhQrhXGoR9o.webp?&width=500",
+    },
+    {
+      id: 16,
+      name: "(style of buckminster fuller)",
+      link: "https://media.seedscienceai.com/media/mx4_F5_xjpSwRhUeqSy1eCi56e4.webp?&width=500",
+    },
+    {
+      id: 17,
+      name: "(style of siya oum)",
+      link: "https://media.seedscienceai.com/media/K4OIYi8dvbpUHz8nDF1cdUEJweE.webp?&width=500",
+    },
+    {
+      id: 18,
+      name: "(style of tim burton)",
+      link: "https://media.seedscienceai.com/media/oSdh5o2jTNT3OCYjresm0OOVYb8.webp?&width=500",
+    },
+    {
+      id: 19,
+      name: "(style of kim jung gi)",
+      link: "https://media.seedscienceai.com/media/f4OHNAlS4CwB_c0YRvPxAzrubSM.webp?&width=500",
+    },
+  ];
+
+  export default artist;
